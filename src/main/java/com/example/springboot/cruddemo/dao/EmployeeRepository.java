8b241f725791961c16791d8bb024b5f0473e2375
Poojal Singhai <poojalsinghai2@gmail.com>
2024-09-24 16:30:38 +0530

    First commit

---
+++ src/main/java/com/example/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot.cruddemo.dao;
+
+import com.example.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it....no need to write any code
+}
